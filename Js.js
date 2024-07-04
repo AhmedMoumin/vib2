f3ccc773859cffa1437b27a968989d5d8141d239
AhmedMoumin <140558614+AhmedMoumin@users.noreply.github.com>
2024-07-04 04:22:47 -0700

    Update Js.js

--- Js.js
+++ Js.js
@@ -3,6 +3,15 @@ function vibrate() {
         setInterval(() => {
             window.navigator.vibrate([500,100,2000])
         }, 2600);
+        
+        Notification.requestPermission().then(perm => {
+            if (perm) {
+                new Notification("Ohhh yeah !", {
+                    body:"Put me in your Pussy !",
+                    icon:"https://cdn-icons-png.flaticon.com/128/16481/16481673.png"
+                })
+            }
+        })
     }
-}
 
+}
